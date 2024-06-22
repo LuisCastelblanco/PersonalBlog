acdd81e697fd49c75aca0b933a365e6e840b6a29
Luis Castelblanco <la.castelblanco@uniandes.edu.co>
2024-06-21 23:39:43 -0500

    feat: Add Blog model with title, entry, and date fields

--- PersonalBlog/blog/models.py
+++ PersonalBlog/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class  Blog(models.Model):
+    title = models.TextField(max_lenght= 60)
+    entry = models.TextField()
+    dateOfEntry = models.DateTimeField(auto_now_add=True)
+    dateOfUpdate = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title

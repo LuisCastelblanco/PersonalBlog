e999270834762c7f45bf6df8761d6d3a200643fe
Luis Castelblanco <la.castelblanco@uniandes.edu.co>
2024-06-24 21:58:10 -0500

    feat: Register Post model in admin site

--- PersonalBlog/blog/models.py
+++ PersonalBlog/blog/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
 
-class  Blog(models.Model):
+class post(models.Model):
     title = models.TextField(max_lenght= 60)
     entry = models.TextField()
     dateOfEntry = models.DateTimeField(auto_now_add=True)
     dateOfUpdate = models.DateTimeField(auto_now=True)
-
     def __str__(self):
         return self.title
+class images(models.Model):
+    post = models.ForeignKey(post, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/')
+    dateOfEntry = models.DateTimeField(auto_now_add=True)
+    dateOfUpdate = models.DateTimeField(auto_now=True)
+
+   
--- PersonalBlog/blog/views.py
+++ PersonalBlog/blog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Blog
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+
+
+def home(request):
+    context = {
+        'posts': Blog.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+class BlogListView(ListView):
+    model = Blog
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-dateOfEntry']
+

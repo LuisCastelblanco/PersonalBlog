fb8d114a126ea35c6fa99b3c13068f02e64bfa51
Luis Castelblanco <la.castelblanco@uniandes.edu.co>
2024-06-25 11:40:39 -0500

    feat: Add BlogDetailView and yearArchiveView to views.py

--- PersonalBlog/blog/views.py
+++ PersonalBlog/blog/views.py
@@ -17,3 +17,22 @@ class BlogListView(ListView):
     context_object_name = 'posts'
     ordering = ['-dateOfEntry']
 
+class BlogDetailView(DetailView):
+    model = Blog
+    template_name = 'blog/post_detail.html'
+
+    def detailView(request, pk):
+        post = Blog.objects.get(pk=pk)
+        return render(request, 'blog/post_detail.html', {'post': post})
+    
+
+
+
+class yearArchiveView(ListView):
+    model = Blog
+    template_name = 'blog/year_archive.html'
+    context_object_name = 'posts'
+    ordering = ['-dateOfEntry']
+    def get_queryset(self):
+        year = self.kwargs.get('year')
+        return Blog.objects.filter(dateOfEntry__year=year)

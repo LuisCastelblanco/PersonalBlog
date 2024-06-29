5117a0fddb8a1a0c2b5f574331982823f3d60bea
Luis Castelblanco <la.castelblanco@uniandes.edu.co>
2024-06-28 22:24:46 -0500

    Refactor import statements in utils.py

--- PersonalBlog/utils.py
+++ PersonalBlog/utils.py
@@ -1,1 +1,8 @@
 from pymongo import MongoClient
+
+def get_db_handle(db_name,host, port, username, password):
+
+    client = MongoClient(host=host, port=port, username=username, password=password)
+
+    db_handle = client['db_name']
+    return db_handle, client

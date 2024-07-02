553185dd9ea9b6aa28d3c968dc17246f1b188f1a
Luis Castelblanco <la.castelblanco@uniandes.edu.co>
2024-07-02 11:47:36 -0500

    Refactor utils.py to remove MongoDB dependency

--- PersonalBlog/blog/models.py
+++ PersonalBlog/blog/models.py
@@ -8,10 +8,17 @@ class post(models.Model):
     dateOfUpdate = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.title
-class images(models.Model):
+    
+class image(models.Model):
     post = models.ForeignKey(post, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='images/')
     dateOfEntry = models.DateTimeField(auto_now_add=True)
     dateOfUpdate = models.DateTimeField(auto_now=True)
-
+    def __str__(self):
+        return self.post.title
+class comment(models.Model):
+    post = models.ForeignKey(post, on_delete=models.CASCADE)
+    name = models.TextField(max_length=100)
+    dateOfComment = models.DateTimeField(auto_now=True)
+    content = models.TextField()
    
--- PersonalBlog/blog/views.py
+++ PersonalBlog/blog/views.py
@@ -2,37 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Blog
 from django.views.generic import ListView, DetailView
+from .utils import get_db_handle 
 
 
+def blog_posts(request):
+    db_handle, client = get_db_handle
+    blog_posts_collection = db_handle['blog_posts']
 
-def home(request):
+    posts = blog_posts_collection.find()
+
+    posts_list = list(posts)
+
     context = {
-        'posts': Blog.objects.all()
+        'posts': posts_list
     }
-    return render(request, 'blog/home.html', context)
 
-class BlogListView(ListView):
-    model = Blog
-    template_name = 'blog/home.html'
-    context_object_name = 'posts'
-    ordering = ['-dateOfEntry']
+    return render(request, 'blog_posts.html', context)
 
-class BlogDetailView(DetailView):
-    model = Blog
-    template_name = 'blog/post_detail.html'
-
-    def detailView(request, pk):
-        post = Blog.objects.get(pk=pk)
-        return render(request, 'blog/post_detail.html', {'post': post})
-    
-
-
-
-class yearArchiveView(ListView):
-    model = Blog
-    template_name = 'blog/year_archive.html'
-    context_object_name = 'posts'
-    ordering = ['-dateOfEntry']
-    def get_queryset(self):
-        year = self.kwargs.get('year')
-        return Blog.objects.filter(dateOfEntry__year=year)
+def create_blog_post(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        

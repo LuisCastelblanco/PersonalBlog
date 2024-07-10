04fba0b5c02ce2e12d192183a6b9ac957754dc19
Luis Castelblanco <la.castelblanco@uniandes.edu.co>
2024-07-09 21:30:06 -0500

    feat: Add 'blog' app to INSTALLED_APPS in settings.py

--- PersonalBlog/PersonalBlog/settings.py
+++ PersonalBlog/PersonalBlog/settings.py
@@ -20,7 +20,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework',
+    'rest_framework'
+    'blog'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

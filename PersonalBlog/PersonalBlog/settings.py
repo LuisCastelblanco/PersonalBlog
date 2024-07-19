d019e77512f2269124a448f10e6fb935232f9c32
Luis Castelblanco <la.castelblanco@uniandes.edu.co>
2024-07-19 11:47:08 -0500

    feat: Add 'corsheaders' app to INSTALLED_APPS and enable CORS middleware
    This commit adds the 'corsheaders' app to the INSTALLED_APPS list in the settings.py file. Additionally, it enables the CORS middleware by adding it to the MIDDLEWARE list. The CORS_ORIGIN_ALLOW_ALL setting is also set to True, allowing all origins to access the API.
    
    Refactor import statements in utils.py

--- PersonalBlog/PersonalBlog/settings.py
+++ PersonalBlog/PersonalBlog/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'blog',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -41,7 +42,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'PersonalBlog.urls'
 

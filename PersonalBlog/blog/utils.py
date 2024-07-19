c14ae8dee543b480b871ff103618ce6819494e7e
Luis Castelblanco <la.castelblanco@uniandes.edu.co>
2024-07-19 11:30:42 -0500

    chore: Refactor utils.py to improve readability and remove unnecessary MongoDB dependency

--- PersonalBlog/blog/utils.py
+++ PersonalBlog/blog/utils.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 from django.conf import settings
 
 def get_db_handle():
-    client = MongoClient(settings.MONGO_URI)
+    client = MongoClient(settings.MONGO_URI
+                        )
     db_handle = client.get_default_database()
     return db_handle, client
